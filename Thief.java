9521eedb9e3e5cb9f4424d91f20dd25865dd893e
lesimongus <sliu60@stuy.edu>
2025-01-10 11:44:13 -0500

    started work on Thief, did the special functions

--- Thief.java
+++ Thief.java
@@ -1,3 +1,26 @@
 public class Thief extends Adventurer{
-  
+  private int stolen, sneaky, maxSneaky;
+
+  public Thief(String name, int hp){
+    super(name, hp);
+    stolen = 0;
+    sneaky = 30;
+    maxSneaky = 30;
+  }
+
+  public String getSpecialName(){
+    return "sneaky";
+  }
+
+  public int getSpecial(){
+    return sneaky;
+  }
+
+  public void setSpecial(int n){
+    sneaky = n;
+  }
+
+  public int getSpecialMax(){
+    return maxSneaky;
+  }
 }
