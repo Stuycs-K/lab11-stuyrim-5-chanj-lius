8a7a6fbf78349cee8dca9e1e4563d1f369bac663
lesimongus <sliu60@stuy.edu>
2025-01-13 11:31:41 -0500

    added a second variable to Thief and made an attack function for Thief

--- Thief.java
+++ Thief.java
@@ -1,5 +1,5 @@
 public class Thief extends Adventurer{
-  private int stolen, sneaky, maxSneaky;
+  private int stolen, maxStolen, sneaky, maxSneaky;
 
   public Thief(String name, int hp){
     super(name, hp);
@@ -32,13 +32,35 @@ public class Thief extends Adventurer{
     return maxSneaky;
   }
 
+  public String getSneakyName(){
+    return "stolen";
+  }
+
+  public int getSneaky(){
+    return sneaky;
+  }
+
+  public void setSneaky(int n){
+    sneaky = n;
+  }
+
+  public int getSneakyMax(){
+    return maxSneaky;
+  }
+
+  @Override
   /*
   all adventurers must have a way to attack enemies and
   support their allys
   */
   //hurt or hinder the target adventurer
   public String attack(Adventurer other){
-
+    int damage = (int)(Math.random() * 10) + 2;
+    other.applyDamage(damage);
+    restoreSpecial(5);
+    setSneaky(damage/2);
+    other.setSpecial(other.getSpecial() - damage/2);
+    return this + "shot " + other + " with their bow and filch their energy! They retreat to the shadows.";
   }
 
   /*This is an example of an improvement that you can make to allow
@@ -59,6 +81,6 @@ public class Thief extends Adventurer{
 
   //hurt or hinder the target adventurer, consume some special resource
   public String specialAttack(Adventurer other){
-    
+
   }
 }
