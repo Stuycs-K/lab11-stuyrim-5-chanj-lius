031022b423ba4128896c12df615285496286b085
Jason Chan <jchan61@stuy.edu>
2025-01-20 00:12:16 -0500

    finialized attack and support functions

--- Boss.java
+++ Boss.java
@@ -1,33 +1,41 @@
 public class Boss extends Adventurer{
-  private int mana, maxMana, sneaky, maxSneaky;
+  private int mana, maxMana;
 
   public Boss(String name, int hp){
     super(name, hp);
     this.mana = 100;
     this.maxMana = 100;
-    this.sneaky = 30;
-    this.maxSneaky = 30;
+  }
+
+  public Boss(String name){
+    this(name, 100);
   }
 
   @Override
   public String attack(Adventurer other){
-    int damage = (int)(Math.random() * 10) + 4;
+    int damage = (int)(Math.random() * 10) + 5;
     other.applyDamage(damage);
-    return "Boss has done " + damage + " damage to " + other;
+    return this + " has used their high IQ to do " + damage + " damage to " + other + "!";
   }
 
   public String support(Adventurer other){
-    this.setSpecial(this.getSpecial() - 5);
-    other.setSpecial(other.getSpecial() + 5);
-    return "Boss has support other";
+    this.restoreSpecial(this.getSpecial() + 5);
+    if (other.getSpecial() - 5 < 0){
+      other.setSpecial(0);
+      return this + " has borrowed all support from " + other "! What a steal!";
+    }
+    else{
+      other.setSpecial(other.getSpecial() - 5);
+      return this + " has borrowed 5 support from " + other "! What a steal!";
+    }
   }
 
   public String support(){
-    this.setSpecial(this.getSpecial() + 5);
-    return "Boss has support self";
+    this.restoreSpecial(this.getSpecial() + 10);
+    return this + " has studied for a second and restored 10 IQ!";
   }
 
   public String specialAttack(Adventurer other){
-    //wizard's special attack?
+
   }
 }
